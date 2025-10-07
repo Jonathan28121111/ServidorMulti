443a74753e5b1f17945ab0829d258514452321ce
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-07 09:04:22 -0700

    Correccion de constructor

--- src/main/java/servidorMulti/ServidorMulti.java
+++ src/main/java/servidorMulti/ServidorMulti.java
@@ -1,37 +1,37 @@
 package servidormulti;
- 
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
- 
+
 public class ServidorMulti {
- 
+
     static HashMap<String, UnCliente> clientes = new HashMap<>();
- 
+
     public static void main(String[] args) {
         int puerto = 8080;
         int contador = 0;
- 
+
         try (ServerSocket servidorSocket = new ServerSocket(puerto)) {
             System.out.println("Servidor iniciado en el puerto " + puerto);
- 
+
             while (true) {
                 Socket socket = servidorSocket.accept();
- 
-                UnCliente uncliente = new UnCliente(socket);
-                Thread hilo = new Thread(uncliente);
- 
+
                 String idCliente = Integer.toString(contador);
+                UnCliente uncliente = new UnCliente(socket, contador);
+                Thread hilo = new Thread(uncliente);
+
                 clientes.put(idCliente, uncliente);
- 
                 hilo.start();
- 
-                System.out.println("se conecto el chango #" + contador);
- 
+
+                System.out.println("Se conecto el cliente #" + contador);
+
                 contador++;
             }
         } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
         }
     }
 }
