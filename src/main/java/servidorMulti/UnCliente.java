5b548d5317c2204044a9b14535c42ffdcd94b745
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-07 08:53:02 -0700

    Correcion para que el mensaje que envie no me aparezca a mi

--- src/main/java/servidorMulti/UnCliente.java
+++ src/main/java/servidorMulti/UnCliente.java
@@ -1,41 +1,56 @@
 package servidormulti;
- 
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
- 
+
 public class UnCliente implements Runnable {
     final DataOutputStream salida;
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     final DataInputStream entrada;
- 
-    UnCliente(Socket s) throws IOException {
+    private final int miId;
+
+    UnCliente(Socket s, int id) throws IOException {
+        this.miId = id;
         salida = new DataOutputStream(s.getOutputStream());
         entrada = new DataInputStream(s.getInputStream());
     }
- 
+
     @Override
     public void run() {
         while (true) {
             try {
                 String mensaje = entrada.readUTF();
- 
                 if (mensaje.startsWith("@")) {
                     String[] partes = mensaje.split(" ", 2);
                     String aQuien = partes[0].substring(1);
+                    
                     UnCliente cliente = ServidorMulti.clientes.get(aQuien);
-                    cliente.salida.writeUTF(mensaje);
-                    return;
+                    
+                    if (cliente != null) {
+                        cliente.salida.writeUTF("[De #" + miId + "]: " + mensaje);
+                        cliente.salida.flush();
+                    } else {
+                        salida.writeUTF("Cliente #" + aQuien + " no existe");
+                        salida.flush();
+                    }
+                    continue;
                 }
- 
+
                 for (UnCliente cliente : ServidorMulti.clientes.values()) {
-                    cliente.salida.writeUTF(mensaje);
+                    if (cliente.miId != this.miId) {
+                        cliente.salida.writeUTF("[Cliente #" + miId + "]: " + mensaje);
+                        cliente.salida.flush();
+                    }
                 }
- 
+
             } catch (IOException ex) {
+                System.out.println("Cliente #" + miId + " desconectado");
+                ServidorMulti.clientes.remove(Integer.toString(miId));
+                break;
             }
         }
     }
