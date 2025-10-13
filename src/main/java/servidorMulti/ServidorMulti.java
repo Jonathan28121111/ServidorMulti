876d2ea97ad0b2724721c4c361e353216e247773
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-12 18:41:30 -0700

    ServidorMulti actualizado mandar tres mensaje antes de iniciar sesion

--- src/main/java/servidorMulti/ServidorMulti.java
+++ src/main/java/servidorMulti/ServidorMulti.java
@@ -4,34 +4,46 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServidorMulti {
-
     static HashMap<String, UnCliente> clientes = new HashMap<>();
-
+    static Map<String, String> usuarios = new HashMap<>(); 
+    
     public static void main(String[] args) {
         int puerto = 8080;
         int contador = 0;
-
+        
         try (ServerSocket servidorSocket = new ServerSocket(puerto)) {
             System.out.println("Servidor iniciado en el puerto " + puerto);
-
+            
             while (true) {
                 Socket socket = servidorSocket.accept();
-
+                
                 String idCliente = Integer.toString(contador);
-                UnCliente uncliente = new UnCliente(socket, contador);
-                Thread hilo = new Thread(uncliente);
-
-                clientes.put(idCliente, uncliente);
+                UnCliente unCliente = new UnCliente(socket, contador);
+                Thread hilo = new Thread(unCliente);
+                
+                clientes.put(idCliente, unCliente);
                 hilo.start();
-
-                System.out.println("Se conecto el cliente #" + contador);
-
+                
+                System.out.println("Se conectó el cliente #" + contador);
                 contador++;
             }
         } catch (IOException e) {
             System.out.println("Error en el servidor: " + e.getMessage());
         }
+    }
+    
+    public static boolean registrarUsuario(String usuario, String password) {
+        if (usuarios.containsKey(usuario)) {
+            return false;
+        }
+        usuarios.put(usuario, password);
+        return true;
+    }
+    
+    public static boolean autenticarUsuario(String usuario, String password) {
+        return usuarios.containsKey(usuario) && usuarios.get(usuario).equals(password);
     }
 }
